8b9ac41231aecaac03d6b0c92d141d53f431b62c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-04-18 00:50:59 +0900

    feat: 🎸 DockerfileをHeroku用に調整した

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 set -e
 
-RAILS_PORT=3000
-if [ -n "$PORT" ]; then
-  RAILS_PORT=$PORT
-fi
-
-# migration
-# bin/rails db:migrate RAILS_ENV=production
-
-# assets precompile
-# bundle exec rake assets:precompile RAILS_ENV=production
-
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f tmp/pids/server.pid
+rm -f /myapp/tmp/pids/server.pid
 
-bin/rails s -p $RAILS_PORT -b 0.0.0.0
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
