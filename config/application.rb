515a421bea32d086b5fdfcdcd0de0f665122e945
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-04-18 09:26:57 +0900

    feat: 🎸 許可hostsに ".herokuapp.com" を追加した

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module CloudRunSampleRailsApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.hosts << '.run.app'
+    config.hosts << '.herokuapp.com'
   end
 end
