5439edd794ed920f2f5f1eadd8b3b7e2500883b7
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-02-06 21:38:40 +0900

    feat: 🎸 Update Rails version to 7

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module CloudRunSampleRailsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,6 +18,5 @@ module CloudRunSampleRailsApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << '.run.app'
   end
 end
