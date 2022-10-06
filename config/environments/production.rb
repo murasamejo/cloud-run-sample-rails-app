46e473a61a668fa174e87f15844d8ea800b4eac1
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-10-07 08:31:12 +0900

    fix: 🐛 production環境 の config.hosts をさらにさらに修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Cloud Run でアクセスできるようにする
-  config.hosts << '*.run.app'
+  config.hosts << '**.run.app'
 
   # Settings specified here will take precedence over those in config/application.rb.
 
