a8a980d522fb6e36dae6a3e764e5727fba9f1863
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-10-10 17:12:15 +0900

    refactor: 💡 余剰な記述をリファクタリングした (#54)

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ if [ -n "$PORT" ]; then
   RAILS_PORT=$PORT
 fi
 
-RAILS_ENV=${RAILS_ENV:-development} bin/rails assets:precompile
-RAILS_ENV=${RAILS_ENV:-development} bin/rails db:prepare
-RAILS_ENV=${RAILS_ENV:-development} bin/rails db:seed
+bin/rails assets:precompile
+bin/rails db:prepare
+bin/rails db:seed
 
 rm -f tmp/pids/server.pid
 
-RAILS_ENV=${RAILS_ENV:-development} bin/rails s -p $PORT -b 0.0.0.0
+bin/rails s -p $PORT -b 0.0.0.0
