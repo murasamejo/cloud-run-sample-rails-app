10ebf90259530c2b61bd3de7f5d36b5cea765067
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-11-19 21:40:35 +0900

    refactor: 💡 db:migrate などは entrypoint.sh 内でちゃんとやられていたので CI から削除

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,10 @@ if [ -n "$PORT" ]; then
   RAILS_PORT=$PORT
 fi
 
+# production デプロイ時には RAILS_ENV の指定を忘れないこと
 bin/rails assets:precompile
-bin/rails db:prepare
+bin/rails db:create
+bin/rails db:migrate
 bin/rails db:seed
 
 rm -f tmp/pids/server.pid
